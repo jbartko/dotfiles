dfaacce9ac810056b9382df7141285304166acf5
John Bartko <john.bartko@drillinginfo.com>
2016-12-27 14:28:06 -0600

    handle distro-specific package names

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -2,19 +2,20 @@ case node['platform_family']
 when 'fedora'
   include_recipe 'yum::dnf_yum_compat'
   include_recipe 'bartko_wants::fedora_repos'
+  distro_packages = ['nmap-ncat','vim-enhanced']
 when 'debian'
   include_recipe 'apt'
   include_recipe 'bartko_wants::ubuntu_repos'
+  distro_packages = ['netcat','vim']
 end
 
-package %w{
+package distro_packages + %w{
   atom
   atop
   git
   google-chrome-stable
   hipchat4
   htop
-  nc
   ncdu
   nmap
   jq
