4313f5239f6989cf6d703edbbc0c7c7637ae8122
John Bartko <john.bartko@drillinginfo.com>
2016-12-28 13:45:52 -0600

    specify Docker version

--- chef/recipes/docker.rb
+++ chef/recipes/docker.rb
@@ -1,5 +1,7 @@
 if node['platform_family'] == 'fedora' && node['platform_version'].to_i == 25
   docker_installation_package 'default' do
+    package_name 'docker'
+    version '1.12.5'
     action :create
   end
 else
